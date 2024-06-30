ff0706b5f652d1c6338ba0da5f9ec078fe809001
hanzch84 <73715848+hanzch84@users.noreply.github.com>
2024-06-30 19:48:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ openai.api_key = api_key
 # ChatGPT API 호출 함수
 def generate_script_with_gpt(grade, num_people, duration, key_phrases, key_words):
     response = openai.ChatCompletion.create(
-        model="gpt-4o",
+        model="gpt-4",
         messages=[
             {"role": "system",
              "content": "You are a skilled playwright specializing in role-playing scripts as a teacher. You excel at writing scripts with easy words, especially for elementary school students. You can write engaging role-play scripts using educationally appropriate words and situations that help students learn key expressions in a fun way."},
@@ -45,25 +45,26 @@ def download_script(script):
 st.title("초등 영어 상황극 대본 생성기")
 col1, col2, col3 = st.columns(3)
 
-grade = col1.selectbox("학년", ["1", "2", "3", "4", "5", "6"], index=5) 
+grade = col1.selectbox("학년", ["1", "2", "3", "4", "5", "6"], index=5)
 num_people = col2.slider("상황극 인원", min_value=2, max_value=10, value=4)
 duration = col3.slider("길이(초)", min_value=10, max_value=300, value=30)
 key_phrases = st.text_input("주요 표현 입력")
 key_words = st.text_area("주요 단어 입력")
 
-
-
+if 'script' not in st.session_state:
+    st.session_state['script'] = ""
 
 if st.button("상황극 대본 생성"):
-    script = generate_script_with_gpt(grade, num_people, duration, key_phrases, key_words)
-    st.markdown(f"### 상황극 대본\n\n{script}")
+    st.session_state['script'] = generate_script_with_gpt(grade, num_people, duration, key_phrases, key_words)
+    st.markdown(f"### 상황극 대본\n\n{st.session_state['script']}")
 
+if st.session_state['script']:
     if st.button("음성파일 다운로드"):
-        audio_file = download_audio(script)
+        audio_file = download_audio(st.session_state['script'])
         with open(audio_file, "rb") as file:
             st.download_button(label="Download audio", data=file, file_name="script_audio.mp3", mime="audio/mp3")
 
     if st.button("대본 다운로드"):
-        script_file = download_script(script)
+        script_file = download_script(st.session_state['script'])
         with open(script_file, "rb") as file:
             st.download_button(label="Download script", data=file, file_name="script.txt", mime="text/plain")
